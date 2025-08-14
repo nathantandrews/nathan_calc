7bbf14790760dc47b1cda942ef051c2396a76dee
Nathan Andrews <nathantandrews@gmail.com>
2025-08-14 12:43:50 -0700

    refactored naming for ui class

--- include/ui.h
+++ include/ui.h
@@ -5,14 +5,14 @@
 
 typedef struct input_box
 {
-    char input_box_str[MAX_INPUT_SIZE];
-    char last_input_box_str[MAX_INPUT_SIZE];
+    char buf[MAX_INPUT_SIZE];
+    char last_buf[MAX_INPUT_SIZE];
     int len;
 } input_box;
 
-input_box* input_box_create(void);
+input_box* ui_input_box_create(void);
 
-void ui_set_screen_text(char* text);
-void ui_display_result(float result);
+void ui_input_box_set_str(const char text[]);
+void ui_input_box_set_float(float result);
 
 #endif
--- src/ui_gtk.c
+++ src/ui_gtk.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "ui.h"
 
-input_box* input_box_create(void)
+input_box* ui_input_box_create(void)
 {
     input_box* new_input_box = (input_box*) malloc(sizeof(*new_input_box));
     memset(new_input_box, 0, sizeof(*new_input_box));
     return new_input_box;
 }
 
-void ui_set_screen_text(char* text)
+void ui_input_box_set_str(const char text[])
 {
-    printf("setting screen text to %s\n", &text);
+    printf("setting screen text to %s\n", text);
 }
 
-void ui_display_result(float result)
+void ui_input_box_set_float(float result)
 {
     printf("printing result: %f\n", result);
 }
